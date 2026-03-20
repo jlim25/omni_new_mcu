cf50de3cd5294d53df17da92e61a3b20b19a88f2
Jacky <jackylim1301@gmail.com>
2026-03-20 01:03:36 -0400

    nit

--- Controls/jointControlmodified.py
+++ Controls/jointControlmodified.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 
-#import ServoControl
+import ServoControl
 
 
 DEFAULT_MOVE_MS = 1000
